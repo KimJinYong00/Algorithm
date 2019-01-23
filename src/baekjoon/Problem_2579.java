18906550be5f1abb400bc4b3e83a186740ee15c1
KimJinYong00 <10ok10@naver.com>
2019-01-24 02:02:44 +0900

    dp 이용

---
+++ src/baekjoon/Problem_2579.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+import util.Problem;
+
+public class Problem_2579 implements Problem {
+
+	
+	/*
+	 * 2579 계단오르기 (https://www.acmicpc.net/problem/2579)
+	 */
+	public static void main(String[] args) {
+		Problem_2579 problem = new Problem_2579();
+
+		problem.solve();
+	}
+	
+	@Override
+	public void solve() {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int[] cost = new int[301];
+		int[] DP =new int[301];
+		
+		for(int i = 1; i <= N; i++) {
+			cost[i] = scan.nextInt();
+		}
+		
+		DP[1] = cost[1];
+		DP[2] = getBiggerInt(cost[2], cost[2] + cost[1]);
+		DP[3] = getBiggerInt(cost[2] + cost[3], cost[1] + cost[3]);
+		for(int i = 4; i <= N; i++) {
+			DP[i] = getBiggerInt(DP[i - 2] + cost[i], DP[i - 3] + cost[i - 1] + cost[i]);
+		}
+		
+		System.out.println(DP[N]);
+		scan.close(); 
+	}
+	
+	private static int getBiggerInt(int a, int b) {
+		return a > b ? a : b;
+	}
+}
